145a355905bab57f7c49e4471df0302b14d74c8c
DanishAnsari5522 <ddanish1928@gmail.com>
2022-11-30 22:27:23 +0530

    Guessing game

---
+++ easy.js
@@ -0,0 +1,12 @@
+// var value=document.getElementsByClassName("entre_guess").value;
+// document.write(value);
+
+var random=Math.floor(Math.random()*3);
+document.write(random);
+
+var ds=document.getElementsByClassName("chek");
+ds.addEventListener("onclick", function(){
+    // var dd=document.getElementsByClassName("entre_guess").value;
+    // document.write(dd);
+    alert("hii");
+})
